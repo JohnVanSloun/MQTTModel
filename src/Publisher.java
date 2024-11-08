1e22a32400d039506106f445d38b1ac2f2d5504c
John Van Sloun <jpvansloun@gmail.com>
2024-11-07 22:00:54 -0600

    populated Publisher class with fields and method headers

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,5 +1,43 @@
-package mqtt/src
+package mqtt.src;
+
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 
 public class Publisher {
-    
+    private Socket socket = null;
+    private String name;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Connects to a server and sets the instance's socket field.
+     * 
+     * param: host is the host ip address to connect to.
+     * param: port is the port number to connect the socket to.
+     */
+    public void connect(String host, int port) {
+        return;
+    }
+
+    /**
+     * Sends a message over the socket to the server.
+     * 
+     * param: message is the message that is to be sent to the server.
+     */
+    public void sendMessage(String message) {
+        return;
+    }
+
+    /**
+     * Disconnects from the server.
+     */
+    public void disconnect() {
+        return;
+    }
+
 }
