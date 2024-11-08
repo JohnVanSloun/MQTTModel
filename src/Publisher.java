af5f9e45d6c7cb16cd635ec5c792753448bcbceb
John Van Sloun <jpvansloun@gmail.com>
2024-11-07 23:52:44 -0600

    completed functionality to connect and send a message

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,14 +1,16 @@
-package mqtt.src;
-
 import java.net.Socket;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Publisher {
-    private Socket socket = null;
     private String name;
-    private PrintWriter out = null;
-    private BufferedReader in = null;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
 
     public Publisher(String name) {
         this.name = name;
@@ -20,8 +22,23 @@ public class Publisher {
      * param: host is the host ip address to connect to.
      * param: port is the port number to connect the socket to.
      */
-    public void connect(String host, int port) {
-        return;
+    public void connect(InetAddress host, int port) {
+        try {
+            socket = new Socket(host, port);
+            System.out.println("Connected");
+
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.write("<" + this.name + ",CONN>\n");
+
+            System.out.println("Client closing");
+            out.close();
+            in.close();
+            socket.close();
+        } catch(IOException e) {
+            System.out.println("Error opening a connection with the server.");
+        }
     }
 
     /**
@@ -40,4 +57,13 @@ public class Publisher {
         return;
     }
 
+    public static void main(String[] args) {
+        try {
+            Publisher publisher = new Publisher("pub1");
+            publisher.connect(InetAddress.getLocalHost(), 4444);
+        } catch(UnknownHostException e) {
+            System.out.println("Host not known");
+        }
+        
+    }
 }
